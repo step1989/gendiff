0fcb6750612af13d7a1d5295bc082c6137208fba
Ryabukhin Stepan <89823401445@yandex.ru>
2020-01-22 13:39:38 +0500

    рефакторинг теста

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,19 +1,19 @@
+import fs from 'fs';
+import path from 'path';
 import gendiff from '../src';
-
-const fs = require('fs');
-const path = require('path');
 
 const pathdir = '/__fixtures__/tree/';
 
-describe.each(['pretty', 'plain', 'json'])('test %s format', (outputFormat) => {
-  describe.each(['json', 'ini', 'yaml'])('test comparator', (extensions) => {
-    const expectedPath = path.join(__dirname, `${pathdir}${outputFormat}`);
-    const expected = fs.readFileSync(expectedPath, 'utf8');
+describe.each(['pretty', 'plain', 'json'])('test %s output format', (outputFormat) => {
+  const expectedPath = path.join(__dirname, `${pathdir}${outputFormat}`);
+  const expected = fs.readFileSync(expectedPath, 'utf8');
+
+  describe.each(['json', 'ini', 'yaml'])('test gendiff', (extensions) => {
     const beforeFilePath = path.join(__dirname, `${pathdir}before.${extensions}`);
     const afterFilePath = path.join(__dirname, `${pathdir}after.${extensions}`);
     const received = gendiff(beforeFilePath, afterFilePath, outputFormat);
 
-    test(`Test compare ${extensions} file. Return ${outputFormat}`, () => {
+    test(`Test compare ${extensions} file.`, () => {
       expect(received).toEqual(expected);
     });
   });
