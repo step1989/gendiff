602685598976d8fab23ebccf81e957da38ad615a
Ryabukhin Stepan <89823401445@yandex.ru>
2020-01-22 14:31:28 +0500

    добавлена возможность использования приложения как библиотеки

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,7 +3,7 @@
 import program from 'commander';
 import gendiff from '..';
 
-program.version('1.0.0')
+program.version('1.0.1')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format (pretty, plain, json). Default output pretty')
   .arguments('<firstConfig>')
