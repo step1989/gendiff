089b34ab4ee52a67b9af1a420fd669f20affcdc6
Ryabukhin Stepan <89823401445@yandex.ru>
2020-01-26 22:57:40 +0500

    выделена функция для получения пути сравниваемых файлов. Исправлены ошибки именования

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,16 +4,19 @@ import gendiff from '../src';
 
 const pathdir = '/__fixtures__/tree/';
 
+const getPathFile = (fileName, extension = 'txt') => path.join(__dirname, `${pathdir}${fileName}.${extension}`);
+
 describe.each(['pretty', 'plain', 'json'])('test %s output format', (outputFormat) => {
-  const expectedPath = path.join(__dirname, `${pathdir}${outputFormat}`);
+  const resultFileName = outputFormat;
+  const expectedPath = getPathFile(resultFileName);
   const expected = fs.readFileSync(expectedPath, 'utf8');
 
-  describe.each(['json', 'ini', 'yaml'])('test gendiff', (extensions) => {
-    const beforeFilePath = path.join(__dirname, `${pathdir}before.${extensions}`);
-    const afterFilePath = path.join(__dirname, `${pathdir}after.${extensions}`);
+  describe.each(['json', 'ini', 'yaml'])('test gendiff', (extension) => {
+    const beforeFilePath = getPathFile('before', extension);
+    const afterFilePath = getPathFile('after', extension);
     const received = gendiff(beforeFilePath, afterFilePath, outputFormat);
 
-    test(`Test compare ${extensions} file.`, () => {
+    test(`Test compare ${extension} file.`, () => {
       expect(received).toEqual(expected);
     });
   });
